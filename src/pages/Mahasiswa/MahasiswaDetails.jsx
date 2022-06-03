7770828f3c28455af479ff3f4f4e27770b9aed66
jdkhusuma <juan.d.khusuma@gmail.com>
2022-06-03 12:35:21 +0700

    Added schedule table display to mahasiswa details

--- src/pages/Mahasiswa/MahasiswaDetails.jsx
+++ src/pages/Mahasiswa/MahasiswaDetails.jsx
@@ -14,38 +14,20 @@ export default function MahasiswaDetails() {
   const [loading, setLoading] = React.useState(true);
   const [changed, setChanged] = React.useState(false);
   const [error, setError] = React.useState("");
-  const [mataKuliahSenin, setMataKuliahSenin] = React.useState([]);
-  const [mataKuliahSelasa, setMataKuliahSelasa] = React.useState([]);
-  const [mataKuliahRabu, setMataKuliahRabu] = React.useState([]);
-  const [mataKuliahKamis, setMataKuliahKamis] = React.useState([]);
-  const [mataKuliahJumat, setMataKuliahJumat] = React.useState([]);
-  const [mataKuliahSabtu, setMataKuliahSabtu] = React.useState([]);
-  const [mataKuliahMinggu, setMataKuliahMinggu] = React.useState([]);
+  const [schedule, setSchedule] = React.useState([]);
 
   React.useEffect(() => {
-    setMataKuliahSenin((_) => []);
-    setMataKuliahSelasa((_) => []);
-    setMataKuliahRabu((_) => []);
-    setMataKuliahKamis((_) => []);
-    setMataKuliahJumat((_) => []);
-    setMataKuliahSabtu((_) => []);
-    setMataKuliahMinggu((_) => []);
     if (mahasiswa) {
+      const schedule = {};
       for (const mataKuliah of mahasiswa.MataKuliah) {
-        mataKuliah.jadwal.includes("SENIN") &&
-          setMataKuliahSenin((prevState) => [...prevState, mataKuliah]);
-        mataKuliah.jadwal.includes("SELASA") &&
-          setMataKuliahSelasa((prevState) => [...prevState, mataKuliah]);
-        mataKuliah.jadwal.includes("RABU") &&
-          setMataKuliahRabu((prevState) => [...prevState, mataKuliah]);
-        mataKuliah.jadwal.includes("KAMIS") &&
-          setMataKuliahKamis((prevState) => [...prevState, mataKuliah]);
-        mataKuliah.jadwal.includes("JUMAT") &&
-          setMataKuliahJumat((prevState) => [...prevState, mataKuliah]);
-        mataKuliah.jadwal.includes("SABTU") &&
-          setMataKuliahSabtu((prevState) => [...prevState, mataKuliah]);
-        mataKuliah.jadwal.includes("MINGGU") &&
-          setMataKuliahMinggu((prevState) => [...prevState, mataKuliah]);
+        for (const jadwal of mataKuliah.jadwal) {
+          if (!schedule[jadwal]) {
+            schedule[jadwal] = [];
+          }
+          schedule[jadwal].push(mataKuliah);
+        }
+        console.log(schedule);
+        setSchedule((_) => schedule);
       }
     }
   }, [mahasiswa]);
@@ -70,119 +52,145 @@ export default function MahasiswaDetails() {
       : setLoading(false);
   }, [user]);
   return (
-    <div className="flex flex-col w-full h-[100vh] items-center justify-center">
+    <div className="flex flex-col w-full h-[100vh] items-center justify-center overflow-x-auto">
       {error && (
         <div className="font-bold text-red-600 text-center">{error}</div>
       )}
       {user ? (
         !loading ? (
-          <form>
-            {mahasiswa ? (
-              <table className="">
-                <tbody>
-                  <tr>
-                    <td className="font-bold px-4 py-2">Name</td>
-                    <td className="px-6">
-                      <input
-                        className="border m-2 px-2 py-1 bg-stone-300"
-                        type="text"
-                        value={name}
-                        onChange={(e) =>
-                          setName(e.target.value) || setChanged(true)
-                        }
-                      />
-                    </td>
-                  </tr>
-                  <tr>
-                    <td className="font-bold px-4 py-2">NIM</td>
-                    <td className="px-6">
-                      <input
-                        className="border m-2 px-2 py-1 bg-stone-300"
-                        type="text"
-                        value={NIM}
-                        onChange={(e) =>
-                          setNIM(e.target.value) || setChanged(true)
-                        }
-                      />
-                    </td>
-                  </tr>
-                </tbody>
-              </table>
-            ) : (
-              <div className="font-bold">User Not Found</div>
-            )}
-            <div className="text-center">
-              <div
-                className="cursor-pointer border hover:bg-blue-400  border-black px-4 py-2 font-bold hover:text-white"
-                onClick={() => navigate("/mahasiswa")}
-              >
-                Go Back
-              </div>
-              <div
-                className="cursor-pointer border hover:bg-yellow-400 border-black px-4 py-2 font-bold hover:text-white"
-                onClick={() => {
-                  setName(mahasiswa.name);
-                  setNIM(mahasiswa.NIM);
-                  setChanged(false);
-                }}
-              >
-                Reset
-              </div>
-              {changed ? (
-                <div
-                  className="cursor-pointer border hover:bg-green-500 border-black px-4 py-2 font-bold hover:text-white"
-                  onClick={() => {
-                    let data = { name, NIM, username, Role: role };
-                    for (const key in data) {
-                      if (data[key] === mahasiswa[key]) {
-                        delete data[key];
-                      }
-                    }
-                    fetch(`${config.url}/mahasiswa/${id}`, {
-                      method: "PUT",
-                      headers: {
-                        Authorization: `Bearer ${localStorage.getItem(
-                          "auth-token"
-                        )}`,
-                        "Content-Type": "application/json",
-                      },
-                      body: JSON.stringify({ ...data }),
-                    }).then((res) =>
-                      res.status === 200
-                        ? navigate("/mahasiswa")
-                        : res.json().then((res) => {
-                            setError(res.error.message);
-                          })
-                    );
-                  }}
-                >
-                  Save
-                </div>
+          <>
+            <form>
+              {mahasiswa ? (
+                <table className="">
+                  <tbody>
+                    <tr>
+                      <td className="font-bold px-4 py-2">Name</td>
+                      <td className="px-6">
+                        <input
+                          className="border m-2 px-2 py-1 bg-stone-300"
+                          type="text"
+                          value={name}
+                          onChange={(e) =>
+                            setName(e.target.value) || setChanged(true)
+                          }
+                        />
+                      </td>
+                    </tr>
+                    <tr>
+                      <td className="font-bold px-4 py-2">NIM</td>
+                      <td className="px-6">
+                        <input
+                          className="border m-2 px-2 py-1 bg-stone-300"
+                          type="text"
+                          value={NIM}
+                          onChange={(e) =>
+                            setNIM(e.target.value) || setChanged(true)
+                          }
+                        />
+                      </td>
+                    </tr>
+                  </tbody>
+                </table>
               ) : (
+                <div className="font-bold">User Not Found</div>
+              )}
+              <div className="text-center">
                 <div
-                  className="cursor-pointer border hover:bg-red-600 border-black px-4 py-2 font-bold hover:text-white"
+                  className="cursor-pointer border hover:bg-blue-400  border-black px-4 py-2 font-bold hover:text-white"
+                  onClick={() => navigate("/mahasiswa")}
+                >
+                  Go Back
+                </div>
+                <div
+                  className="cursor-pointer border hover:bg-yellow-400 border-black px-4 py-2 font-bold hover:text-white"
                   onClick={() => {
-                    fetch(`${config.url}/mahasiswa/${id}`, {
-                      method: "DELETE",
-                      headers: {
-                        Authorization: `Bearer ${localStorage.getItem(
-                          "auth-token"
-                        )}`,
-                      },
-                    }).then((res) =>
-                      res.status === 200
-                        ? navigate("/mahasiswa")
-                        : res.json().then((res) => {
-                            setError(res.error.message);
-                          })
-                    );
+                    setName(mahasiswa.name);
+                    setNIM(mahasiswa.NIM);
+                    setChanged(false);
                   }}
                 >
-                  Delete Record
+                  Reset
                 </div>
-              )}
-            </div>
-          </form>
+                {changed ? (
+                  <div
+                    className="cursor-pointer border hover:bg-green-500 border-black px-4 py-2 font-bold hover:text-white"
+                    onClick={() => {
+                      let data = { name, NIM, username, Role: role };
+                      for (const key in data) {
+                        if (data[key] === mahasiswa[key]) {
+                          delete data[key];
+                        }
+                      }
+                      fetch(`${config.url}/mahasiswa/${id}`, {
+                        method: "PUT",
+                        headers: {
+                          Authorization: `Bearer ${localStorage.getItem(
+                            "auth-token"
+                          )}`,
+                          "Content-Type": "application/json",
+                        },
+                        body: JSON.stringify({ ...data }),
+                      }).then((res) =>
+                        res.status === 200
+                          ? navigate("/mahasiswa")
+                          : res.json().then((res) => {
+                              setError(res.error.message);
+                            })
+                      );
+                    }}
+                  >
+                    Save
+                  </div>
+                ) : (
+                  <div
+                    className="cursor-pointer border hover:bg-red-600 border-black px-4 py-2 font-bold hover:text-white"
+                    onClick={() => {
+                      fetch(`${config.url}/mahasiswa/${id}`, {
+                        method: "DELETE",
+                        headers: {
+                          Authorization: `Bearer ${localStorage.getItem(
+                            "auth-token"
+                          )}`,
+                        },
+                      }).then((res) =>
+                        res.status === 200
+                          ? navigate("/mahasiswa")
+                          : res.json().then((res) => {
+                              setError(res.error.message);
+                            })
+                      );
+                    }}
+                  >
+                    Delete Record
+                  </div>
+                )}
+              </div>
+            </form>
+            {Object.keys(schedule).length !== 0 && (
+              <>
+                <h1 className="font-bold mb-3 mt-16">Jadwal</h1>
+                <table>
+                  <tbody>
+                    {Object.keys(schedule).map((key) => (
+                      <tr key={key}>
+                        <td className="font-bold px-4 py-2">{key}</td>
+                        {schedule[key].map((mataKuliah) => (
+                          <td
+                            className="px-6  cursor-pointer"
+                            onClick={() =>
+                              navigate(`/mata-kuliah/${mataKuliah.id}`)
+                            }
+                          >
+                            {mataKuliah.code}
+                          </td>
+                        ))}
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
+              </>
+            )}
+          </>
         ) : (
           <div className="font-bold">Loading...</div>
         )
